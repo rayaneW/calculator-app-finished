d618a363ae7b193db2b566b88f2f45bd7397d883
NotMatthias <matthias.vandeuren@student.pxl.be>
2023-12-09 15:37:20 +0100

    code voor extra opdracht

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const app = require("./app");
-const port = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+const ip = require('ip');
+
+dotenv.config();
+
+
+const port = getServerPort();
 
 app.listen(port);
 console.log("Server running on port: " + port);
+
+function getServerPort() {
+    const serverIp = ip.address();
+
+    if (serverIp === '192.168.6.138') {
+        return process.env.PORT_TEST || 4000;
+    } else if (serverIp === '192.168.6.139') {
+        return process.env.PORT_PROD || 80;
+    } else {
+        return process.env.PORT || 3000;
+    }
+}
